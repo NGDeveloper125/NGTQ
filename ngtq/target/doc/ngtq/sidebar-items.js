3d62c117970811581cabca89eb558b8fc3f69b9c
NGDeveloper125 <NGDeveloper125@outlook.com>
2025-04-25 17:43:49 +0100

    Add cargo docs & move readme and license file into project root folder

---
+++ ngtq/target/doc/ngtq/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":["NGTQErrorType"],"struct":["NGTQError"],"trait":["NGTQ"]};
---
+++ ngtq/target/doc/src-files.js
@@ -0,0 +1,4 @@
+var srcIndex = new Map(JSON.parse('[\
+["ngtq",["",[],["lib.rs","ngtq_error.rs"]]]\
+]'));
+createSrcSidebar();
