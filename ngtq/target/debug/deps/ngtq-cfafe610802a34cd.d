a3e6be34edd24e3090868face1f7e597da2e8acd
NGDeveloper125 <NGDeveloper125@outlook.com>
2025-04-25 17:58:54 +0100

    add config toml file for crate

--- ngtq/target/debug/deps/ngtq-cfafe610802a34cd.d
+++
@@ -1,6 +0,0 @@
-/home/ngdeveloper/NGTQ/ngtq/target/debug/deps/libngtq-cfafe610802a34cd.rmeta: src/lib.rs src/ngtq_error.rs
-
-/home/ngdeveloper/NGTQ/ngtq/target/debug/deps/ngtq-cfafe610802a34cd.d: src/lib.rs src/ngtq_error.rs
-
-src/lib.rs:
-src/ngtq_error.rs:
--- ngtq/target/debug/libngtq.d
+++
@@ -1,1 +0,0 @@
-/home/ngdeveloper/NGTQ/ngtq/target/debug/libngtq.rlib: /home/ngdeveloper/NGTQ/ngtq/src/lib.rs /home/ngdeveloper/NGTQ/ngtq/src/ngtq_error.rs
