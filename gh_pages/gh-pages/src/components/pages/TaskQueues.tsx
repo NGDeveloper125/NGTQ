b57078ab43014326f308f89283a5d19c1f04c645
NGDeveloper125 <NGDeveloper125@outlook.com>
2025-05-08 21:03:38 +0100

    Add basic content

--- gh_pages/gh-pages/src/components/pages/TaskQueues.tsx
+++ gh_pages/gh-pages/src/components/pages/TaskQueues.tsx
@@ -3,7 +3,15 @@
 function TaskQueues() {
     return (
         <div>
-            <h1>Task-Queues Page</h1>
+            <h1>Task-Queues</h1>
+            <p>
+                In this section you can find all the current task queue implementations of the NGTQ trait.<br/>
+                This task queues can be uses in monolitic applications or as the domain of a communication service like message buses,<br/>
+                Each contains different features and are better for different scnarios.<br/>
+                You can find implementaion instructions, pros and cons and banchmark for each task queue. <br/>   
+            </p>
+            <h3>Current Task Queues Implementations</h3>
+            
         </div>
     );
 }
