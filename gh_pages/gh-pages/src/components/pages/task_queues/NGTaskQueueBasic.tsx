56e3c1883909091a582912472fcbfa94c747ebb6
NGDeveloper125 <NGDeveloper125@outlook.com>
2025-05-09 22:05:05 +0100

    Add TaskQueues page content & TaskQueue object

--- gh_pages/gh-pages/src/components/pages/task_queues/NGTaskQueueBasic.tsx
+++ gh_pages/gh-pages/src/components/pages/task_queues/NGTaskQueueBasic.tsx
@@ -1,11 +1,44 @@
+import { useState } from "react";
 import "../../../styles/pages/TaskQueue.css"
 
 function NGTaskQueueBasic() {
+    let [expend, setExpend] = useState(false);
     return (
-        <div className="queue-container">
+        <div className={expend ? "queue-container-extended" : "queue-container"}>
             <div className="queue-title-container">
-                <h4 className="queue-title">NGTask Queue Basic</h4>
-                <a className="see-details-btn">See Details</a>
+                <h3 className="queue-title">NGTask Queue Basic</h3>
+                <a className="see-details-btn" onClick={() => setExpend(expend ? false : true)}>{expend ? 'Less Details' : 'More Details'}</a>
+            </div>
+            <div>
+                {expend && (
+                <div className="expended-section-titles-container">
+                    <div className="description-containr">
+                        <h4 className="description-title">Description</h4>
+                        <p className="description-text">
+                            An in memory queues manager, <br />
+                            Allowing simple and fast push and pull <br />
+                            Of tasks and following of the queues size.
+                        </p>
+                    </div>
+                    <div className="include-container">
+                        <h4 className="include-title">Include</h4>
+                        <ul>
+                            <li>In memory queue</li>
+                            <li>Id based queue</li>
+                            <li>Category based queue</li>
+                            <li>Single thread task manager</li>
+                        </ul>
+                    </div>
+                    <div className="not-include-container">
+                        <h4 className="not-include-title">Not-Include</h4>
+                        <ul>
+                            <li>Database based queue</li>
+                            <li>Recover tasks after crash</li>
+                            <li>Multithreaded task manager</li>
+                        </ul>
+                    </div>
+                </div>
+                )}
             </div>
         </div>
     );
